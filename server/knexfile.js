c1a351cf9aa01d68842070f8552df3fca29a33e8
princesso <egbunaoluebube@gmail.com>
2018-10-10 05:37:21 +0100

    knexfile.s from knex init

---
+++ server/knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/todo',
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'todo',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'todo',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
