931381c6fb80dbaa50672059f287a066df899262
princesso <egbunaoluebube@gmail.com>
2018-10-23 04:05:29 +0100

    require and use todo route

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//internal
+var todo = require('./routes/todo');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//internal 
+app.use('/todo',todo)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
